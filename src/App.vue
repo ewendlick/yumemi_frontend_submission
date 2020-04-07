6615ba85f4c2421afd1b1360ef5c9dea09824dd3
Wendlick <ewendlick@gmail.com>
2020-04-07 13:58:07 +0900

    Adds README and skeleton component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PrefecturePopulations />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PrefecturePopulations from './components/PrefecturePopulations.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PrefecturePopulations
   }
 }
 </script>
---
+++ src/components/PrefecturePopulations.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    TODO: label<br>
+    TODO: responsive checkboxes<br>
+    TODO: responsive graph via Highcharts<br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrefecturePopulations',
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+    // TODO: pull the prefecture content
+  },
+  methods: {
+    // TODO: handle change of checkboxes values
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
