e3befcde3a1971fb3b5a5a13be9b8a6fe529063d
Wendlick <ewendlick@gmail.com>
2020-04-07 14:52:25 +0900

    Adds API call for prefecture populations

--- src/components/PrefecturePopulations.vue
+++ src/components/PrefecturePopulations.vue
@@ -4,6 +4,9 @@
     TODO: responsive checkboxes<br>
     {{ prefectures }}<br>
     TODO: responsive graph via Highcharts<br>
+    {{ populationData }}
+
+    <button @click="handleSetPopulationData">Trigger API call</button>
   </div>
 </template>
 
@@ -14,16 +17,19 @@ export default {
   },
   data () {
     return {
-      prefectures: []
+      prefectures: [],
+      populationData: []
     }
   },
   created () {
-    // TODO: pull the prefecture content
+    // TODO: loading spinner
     this.setPrefectures()
     if (this.prefectures.length) {
 
     }
     // TODO: else, error message
+
+    // TODO: end loading spinner
   },
   methods: {
     getPrefectures () {
@@ -39,6 +45,7 @@ export default {
     async setPrefectures () {
       const apiPayload = await this.getPrefectures()
 
+      // TODO: not DRY. Error handling system
       if (apiPayload.data.result) {
         this.prefectures = apiPayload.data.result.map(prefecture => {
           return {
@@ -47,11 +54,50 @@ export default {
           }
         })
       }
-
-      // TODO: check if prefectures are the right format, otherwise throw error
+      // TODO: else, check if prefectures are the right format, otherwise throw error
     },
-    getPopulationData () {
+    getPopulationData (prefectureCodes) {
+      if (prefectureCodes.length === 0) return []
 
+      const primaryPrefectureCode = prefectureCodes[0]
+      const additionalPrefectureCodes = prefectureCodes.slice(1).map(prefectureCode => {
+        return `${prefectureCode}_`
+      }).join(',')
+
+      prefectureCodes = prefectureCodes.join(',')
+      console.log(prefectureCodes)
+      return this.$resas.get(`api/v1/population/composition/perYear?prefCode=${primaryPrefectureCode}&cityCode=-&addArea=${additionalPrefectureCodes}`)
+        .then((response) => {
+          return response
+        })
+        .catch((e) => {
+          // TODO: Error-handling system
+          alert(e)
+        })
+    },
+    async setPopulationData (prefectureCodes) {
+      const apiPayload = await this.getPopulationData(prefectureCodes)
+
+      // TODO: not DRY. Error handling system
+      if (apiPayload.data.result) {
+        this.populationData = apiPayload.data.result
+        /*
+        .map(prefecture => {
+          return {
+            name: prefecture.prefName,
+            value: prefecture.prefCode
+          }
+        })
+        */
+      }
+    },
+    handleSetPopulationData () {
+      // TODO: loading spinner
+
+      // this.setPopulationData([1, 2])
+      this.setPopulationData([1])
+
+      // TODO: end loading spinner
     }
     // TODO: handle change of checkboxes values
   }
